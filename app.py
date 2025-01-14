247267ae2b998eaecdb017a64d1ac97e10f3295d
coogerchou <gz582@nyu.edu>
2025-01-14 15:51:37 +0800

    upgrade v1.1 app.run update

--- app.py
+++ app.py
@@ -260,5 +260,5 @@ def generate_images(user_input):
 #    return "No SVG data found."
 
 if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=80)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
+#    app.run(debug=True)
